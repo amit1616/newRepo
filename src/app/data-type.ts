ad241ce1e8216f7ce610b370ccf72a266bfa869f
Amit Sahu <2270641@tcs.com>
2024-03-28 13:33:15 +0530

    E-COMMERCE-PROJECT

---
+++ src/app/data-type.ts
@@ -0,0 +1,53 @@
+export interface SignUp{
+    name:string,
+    password:string,
+    email:string
+}
+export interface login{
+    email:string,
+    password:string
+    
+}
+export interface product{
+id: number
+    name:string,
+    price:number,
+    category:string,
+    color:string,
+    description:string,
+    image:string,
+    quantity: undefined | number,
+    productId:undefined | number;
+    
+}
+
+export interface cart{
+    id: number |undefined,
+        name:string,
+        price:number,
+        category:string,
+        color:string,
+        description:string,
+        image:string,
+        quantity: undefined | number,
+        userId:number,
+        productId:number;
+        
+    }
+
+    export interface priceSummary{
+        price:number,
+        discount:number,
+        tax:number,
+        delivery:number,
+        total:number
+    }
+
+    export interface order{
+        email:string,
+        address:string,
+        contact:string,
+        totalPrice:number,
+        userId:number,
+        id:number|undefined;
+    }
